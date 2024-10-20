0916213edce7300ede8c7bef44a439260e6a69e9
HOI <leconghoica@gmail.com>
2024-10-20 13:58:33 +0700

    index profile

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -2,14 +2,15 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 
+
 const ProfileScreen = () => {
   return (
     <View>
-      <Text>ProfileScreen</Text>
-      <Link href={'/profile/personal'}>Gooo</Link>
-      <Link href={'profile/changepassword'}>Chnage</Link>
+
+      <Link href={"/profile/personal"}>Gooo</Link>
+      <Link href={"/profile/changepassword"}>Chnage</Link>
     </View>
-  )
+  );
 }
 
 export default ProfileScreen
